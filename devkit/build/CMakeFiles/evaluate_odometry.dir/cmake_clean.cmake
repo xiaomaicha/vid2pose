f53b8cda676b70aee1f0b6fa45db7b95112c83f3
maiqi_wq <240191740@qq.com>
2019-03-15 22:16:10 +0800

    eigen

---
+++ devkit/build/CMakeFiles/evaluate_odometry.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/evaluate_odometry.dir/cpp/evaluate_odometry.cpp.o"
+  "CMakeFiles/evaluate_odometry.dir/cpp/matrix.cpp.o"
+  "evaluate_odometry.pdb"
+  "evaluate_odometry"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/evaluate_odometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
